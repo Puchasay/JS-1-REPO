60467bff98d72c5688781a48d9dde110539b0ff9
Pucha Sayerz <puchastephanie@gmail.com>
2023-11-16 19:35:55 +0100

    did clock & copy lesson 27

--- 26-clock/script.js
+++ 26-clock/script.js
@@ -6,3 +6,34 @@
 // get reference to element with id `clock`
 const clockEl = document.querySelector('#clock');
 
+Date.now();  //ini di ms, sejak thn 1970
+console.log(Date.now());
+
+const now = new Date ();
+now.getHours();
+now.getMinutes();
+now.getSeconds();
+
+now.toLocaleString()
+now.toLocaleDateString();
+now.toLocaleTimeString();
+
+/* setInterval(() => {
+
+    const now = new Date();
+
+    clockEl.innerText = now.toLocaleTimeString();
+}, 1000)
+ */
+//Or write like this:
+
+const tick = () => {
+    const now = new Date();
+
+    clockEl.innerText = now.toLocaleTimeString();
+}
+
+setInterval(tick, 1000);
+
+tick();
+

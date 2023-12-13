3529f51f8493f5ea692f5285cac18d4d98a5f18a
Pucha Sayerz <puchastephanie@gmail.com>
2023-12-13 13:09:20 +0100

    latest update with node module

---
+++ 39-typescript-basics/script.js
@@ -0,0 +1,6 @@
+/**
+ * TypeScript ✍🏻📜
+ *
+ */
+var username = "Mariah Carey";
+var songsTitle = "All I want for christmas is you";
---
+++ 39-typescript-basics/script.ts
@@ -0,0 +1,17 @@
+/**
+ * TypeScript ✍🏻📜
+ *
+ */
+
+let username = "Mariah Carey";
+
+let songsTitle = "All I want for christmas is you";
+let albumSold = 3500;
+
+let x: string;
+
+x = "Tom Jerry";
+x = "50";
+// x = 50;  ga bisa ditulis jd number
+
+export{}
